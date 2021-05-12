43623ac653806eeb04e4a4923860bffb086ec747
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2021-05-12 17:59:42 +0530

    CVT-107 | demo map

--- src/StateData.js
+++ src/StateData.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import StateMap from './StateMap'
 
 const StateData = ({number}) => {
   return (
     <>
-    {console.log(number)}
       <h4>Active Cases: {number.Aurangabad.active}</h4>
       <h4>confirmed Cases: {number.Aurangabad.confirmed}</h4>
       <h4>deceased Cases: {number.Aurangabad.deceased}</h4>
       <h4>recovered Cases: {number.Aurangabad.recovered}</h4>
+      <StateMap/>
     </>
   );
 };
